80ce706a95a0e4f3cd005c1d4d65b605f79a95b7
alienmao <alienmao@users.noreply.github.com>
2021-03-07 05:49:07 +0000

    deployed via Gatsby Publish Action 🎩 for d14b193ed1da928429cd46c141e263764d51d58e

---
+++ component---src-pages-about-js-aa1634a2418b6b3cf31b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(t,e,a){"use strict";a.r(e);var n=a("q1tI"),i=a.n(n),l=a("Bl7J"),o=a("vrFN");e.default=function(t){var e=t.data,a=t.location,n=e.site.siteMetadata.title;return i.a.createElement(l.a,{location:a,title:n},i.a.createElement(o.a,{title:"About Me"}),i.a.createElement("p",null,"懂一点技术的法学生"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-aa1634a2418b6b3cf31b.js.map
